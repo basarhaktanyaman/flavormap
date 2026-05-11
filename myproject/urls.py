ace3ab3494474507c239c0aa319f43c6b451e557
teomanksrc <teomankasarci@hotmail.com>
2026-05-11 20:16:12 +0300

    Revert "FinDish MS3+: auth, CRUD, reviews, favorites, menu, hours, profile, v…"

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,22 +1,7 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import path, include
-
-from myapp import views as myapp_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(
-        "login/",
-        auth_views.LoginView.as_view(template_name="registration/login.html"),
-        name="login",
-    ),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path("register/", myapp_views.register, name="register"),
     path("", include("myapp.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
